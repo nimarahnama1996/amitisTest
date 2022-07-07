a1cf57cee16c6dbfd1fd78faa65418c638e6cfa8
unknown <n.rahnama1996@gmail.com>
2022-07-07 09:43:33 +0430

    navbar responsive

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,14 @@ import React from 'react'
 
 
 import './App.css';
-import Login from './components/Login/Login';
-
+import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
   return (
     
     <div>
-
-      <Login/>
-     
+     <Navbar/>
     </div>
   );
 }
---
+++ my-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {MenuOutlined, BellOutlined } from '@ant-design/icons'
+import { Cascader } from 'antd';
+
+
+import user from '../../img/user.png'
+import './Navbar.css'
+
+
+const options = [
+    {
+      value: 'نام کاربری',
+      label: 'احمد احمدی',
+      
+    },
+  ];
+
+
+
+const Navbar = () => {
+  return (
+    <nav className='nav-center'>
+        <div className='nav-items'>
+        <Cascader size="small" className='nav-cascader' options={options} />
+        <img src={user} className='nav-user' alt='user' />
+        <BellOutlined className='nav-bell' style={{color:'white'}}/>
+        <MenuOutlined style={{marginRight:'15px',marginTop:'37px',color:'white',fontSize:'18px' }} />
+        </div>
+
+
+        
+ 
+        
+    </nav>
+  )
+}
+
+export default Navbar
